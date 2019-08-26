8fe5e538f37ca6f20e3937307e755f91bcbb05ad
Rajarshi Sharma <etarzan@users.noreply.github.com>
2019-08-27 00:58:44 +0530

    added counter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
+import "bootstrap/dist/css/bootstrap.css";
+import Counter from './components/counter';
 
-const element = <h1>Hello World!</h1>
+// const element = <h1>Hello World!</h1>;
 
-ReactDOM.render( element, document.getElementById('root'));  
+ReactDOM.render(<Counter />, document.getElementById("root"));
